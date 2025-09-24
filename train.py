03b533c79c52ed60f24c4a6486f2456efdff6120
GUERROUT <gnazim2001@gmail.com>
2025-09-24 19:20:23 +0200

    Initial commit: projet ML avec MLflow et CI/CD

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
+import os
 import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+
+# Définir un dossier sûr pour les artifacts MLflow
+os.environ["MLFLOW_ARTIFACT_URI"] = "./mlruns"
 
 # Charger dataset
 X, y = load_iris(return_X_y=True)
@@ -18,7 +22,7 @@ y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
 # Logger dans MLflow
-mlflow.set_experiment("iris-classification 2")
+mlflow.set_experiment("iris-classification")
 with mlflow.start_run():
     mlflow.log_param("n_estimators", 400)
     mlflow.log_metric("accuracy", acc)
